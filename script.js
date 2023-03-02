463efe139bbf8fc67a1688ee9bada701d54be16a
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-03-02 20:02:16 +0100

    Add text animation

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ let computerScore = 0;
 let endGame = document.getElementById("end-game");
 let resetButton = document.getElementById("reset-button");
 let game = document.getElementById("game");
+let rpsTitle = document.getElementById("title");
+let scoreText = document.getElementById('score');
+let textTyping = document.getElementById("text-typing");
+
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
+        textTyping.style.display = "none";
         let img = button.querySelector('img');
         playerSelection = img.alt;
         computerSelection = computerPlay();
@@ -33,23 +38,16 @@ function playRound(playerSelection, computerSelection) {
     (computerSelection == "scissors" && playerSelection == "paper") ||
     (computerSelection == "paper" && playerSelection == "rock")) {
         computerScore = ++computerScore;
-        updateComputerScore();
+        updateScore();
     }
     else {
         playerScore = ++playerScore;
-        updatePlayerScore();
+        updateScore();
     }
 }
 
-function updatePlayerScore() {
-    let playerScoreText = document.getElementById('player-score');
-    playerScoreText.textContent = playerScore;
-
-}
-
-function updateComputerScore() {
-    let computerScoreText = document.getElementById('computer-score');
-    computerScoreText.textContent = computerScore;
+function updateScore() {
+    scoreText.textContent = `${playerScore} - ${computerScore}`;
 }
 
 function getWinner() {
@@ -68,16 +66,80 @@ function getWinner() {
 function resetGame() {
     playerScore = 0;
     computerScore = 0;
-    updatePlayerScore();
-    updateComputerScore();
+    updateScore();
     endGame.style.display = "none";
     resetButton.style.display = "none";
+    rpsTitle.style.display = "block";
+    scoreText.style.display = "flex";
     endGame.textContent = "";
     resetButton.textContent = "";
+    textTyping.style.display = "block";
     game.style.display = "flex";
 }
 
 function terminateGame() {
     game.style.display = "none";
+    rpsTitle.style.display = "none";
+    scoreText.style.display = "none";
     resetButton.onclick = () => resetGame();
 }
+
+
+// Animate
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
